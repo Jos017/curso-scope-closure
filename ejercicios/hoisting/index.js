68e12999c999dc7ee0dc7b0bea577968a03529fc
Jos017 <jrbernabe@gmail.com>
2022-06-22 22:27:44 -0400

    Add examples

---
+++ ejercicios/hoisting/index.js
@@ -0,0 +1,11 @@
+// var nameOfDog;  // Undefined Hoisting
+console.log(nameOfDog);
+var nameOfDog = 'Elmo';
+
+nameOfDog();
+
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito';
---
+++ ejercicios/scope/reasign.js
@@ -0,0 +1,33 @@
+// var
+var firstName; // Valor Undefined
+firstName = 'Oscar';
+console.log(firstName);
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Ana';   // reasignar
+console.log(lastName);
+
+var secondName = 'David';   // declarar / asignar
+var secondName = 'Ana'; //redeclarar / reasignar
+console.log(secondName);
+
+
+// let
+let fruit = 'Apple' // Declarar y Asignar
+fruit = 'kiwi'; // reasignar
+
+let fruit = 'Banana';
+
+
+// const
+const animal = 'dog'; // declara y asigna
+animal = 'cat';
+const animal = 'snake';
+console.log(animal);
+
+const vehicles = [];
+vehicles.push('car');
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
